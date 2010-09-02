66b6d10c0152606ec852e2fe5985f67ea03c28f8
thegaffer <thegaffer@tpspencer.com>
2010-09-02 22:45:21 +0000

    Changed namespace to talframework

---
+++ tal-objex-annotations/src/main/java/org/talframework/objexj/annotations/ObjexObj.java
@@ -0,0 +1,20 @@
+package org.talframework.objexj.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation indicates the class is an Objex behaviour
+ * bean and includes as its value the state bean. The Objex
+ * generators then ensures there are appropriate accessor
+ * methods around all the members.
+ * 
+ * @author Tom Spencer
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ObjexObj {
+    Class<?> value();
+}
---
+++ tal-objex-annotations/src/main/java/org/talframework/objexj/annotations/ObjexRefProp.java
@@ -0,0 +1,19 @@
+package org.talframework.objexj.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * 
+ * @author Tom Spencer
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ObjexRefProp {
+    boolean owned() default true;
+    Class<?> type();
+    String newType() default "";
+}
